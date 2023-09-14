1b3b2819ec1373417f14b665b9c474877ca3dc67
flak (home) <flak.yar@gmail.com>
2023-09-14 09:28:06 +0300

    up

--- SeamlessTile.py
+++ SeamlessTile.py
@@ -1,18 +1,21 @@
 import torch
 
+
 class SeamlessTile:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required":
-                    {"model": ("MODEL",),
-                    "tiling": (["enable", "disable"],),
-                    },
-                }
+        return {
+            "required": {
+                "model": ("MODEL",),
+                "tiling": (["enable", "disable"],),
+            },
+        }
 
     CATEGORY = "conditioning"
 
     RETURN_TYPES = ("MODEL",)
     FUNCTION = "run"
+
     def run(self, model, tiling):
         for m in model.model.modules:
             if isinstance(m, torch.nn.Conv2d):
@@ -26,13 +29,18 @@ class SeamlessTile:
 class CircularVAEDecode:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": { "samples": ("LATENT", ), "vae": ("VAE", )}}
+        return {"required": {"samples": ("LATENT",), "vae": ("VAE",)}}
+
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "decode"
 
     CATEGORY = "latent"
 
     def decode(self, vae, samples):
-        for layer in [layer for layer in vae.first_stage_model.modules() if isinstance(layer, torch.nn.Conv2d)]:
-            layer.padding_mode = 'circular'
-        return (vae.decode(samples["samples"]), )
+        for layer in [
+            layer
+            for layer in vae.first_stage_model.modules()
+            if isinstance(layer, torch.nn.Conv2d)
+        ]:
+            layer.padding_mode = "circular"
+        return (vae.decode(samples["samples"]),)
--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
-from .SeamlessTile import SeamlessTile, CircularVAEDecode
+from .SeamlessTile import CircularVAEDecode, SeamlessTile
 
 NODE_CLASS_MAPPINGS = {
     "SeamlessTile": SeamlessTile,
-    "CircularVAEDecode": CircularVAEDecode
+    "CircularVAEDecode": CircularVAEDecode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SeamlessTile": "Seamless Tile"
-    "CircularVAEDecode": "Circular VAE Decode (tile)"
+    "SeamlessTile": "Seamless Tile",
+    "CircularVAEDecode": "Circular VAE Decode (tile)",
 }
 
 __all__ = [NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS]
